94239b7a404815003e0f51485906172547661ed1
moreno227 <psales.erick@gmail.com>
2026-04-08 08:40:32 -0300

    Create Character.kt model class

---
+++ app/src/main/java/com/example/rpgpixel/models/Character.kt
@@ -0,0 +1,14 @@
+data class Character(
+    val id: Int,
+    val name: String,
+    val element: String,
+    val level: Int,
+    val experience: Int,
+    val health: Int,
+    val mana: Int,
+    val strength: Int,
+    val defense: Int,
+    val speed: Int,
+    val intelligence: Int,
+    val description: String
+)
